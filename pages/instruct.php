70cd6a5e8620a1e6ba65357df0a13f9fd2abcc1b
Khamis kaka <67625388+Khamiskaka@users.noreply.github.com>
2025-04-26 16:36:05 +0300

    Add files via upload

---
+++ pages/instruct.php
@@ -0,0 +1,40 @@
+<div class="row">
+    <div class="col-12">
+    <div class="card">
+        <div class="card-header">
+        <h4>Staff Information</h4>
+        <div class="card-header-action">
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#instruction">Add New <i class="fas fa-plus-circle"></i></button>
+        <button type="button" class="btn btn-danger" data-toggle="modal">Show Trash <i class="fas fa-trash-alt"></i></button>
+            </div>
+        </div>
+        <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-striped table-hover" id="save-stage" style="width:100%;">
+            <thead>
+                <tr>
+                <th>Staff Name</th>
+                <th>Position</th>
+                <th>Email</th>
+                <th>Mobile Number</th>
+                <th>Gender</th>
+                <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <td>Khamis Mohamed Khamis</td>
+                <td>Admin</td>
+                <td>khamiskaka199848@gmail.com</td>
+                <td>0629762619</td>
+                <td>Male</td>
+                <td><a href="#"><i class="fas fa-lock"></i></a>  &nbsp; <a href="#" class="text-danger"><i class="fas fa-trash-alt"></i></a>  &nbsp; <a href="#" class="text-warning"><i class="far fa-edit"></i></a> </td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
+        </div>
+    </div>
+    </div>
+</div>
+
---
+++ pages/modal.php
@@ -0,0 +1,363 @@
+<!-- Modal for Staff start here -->
+<div class="modal fade" id="staff" tabindex="-1" role="dialog" aria-labelledby="StaffModal"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="StaffModal">Staff Information</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="form-row">
+            <div class="form-group col-md-4">
+              <label for="inputEmail4">Jina La Kwanza</label>
+              <input type="text" class="form-control" id="first_name" placeholder="Enter First Name">
+            </div>
+            <div class="form-group col-md-4">
+              <label for="inputPassword4">Middle Name</label>
+              <input type="text" class="form-control" id="middle_name" placeholder="Enter Middle Name">
+            </div>
+            <div class="form-group col-md-4">
+              <label for="inputPassword4">Last Name</label>
+              <input type="text" class="form-control" id="last_name" placeholder="Enter Last Name">
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputPassword4">Location</label>
+              <input type="text" class="form-control" id="location" placeholder="Enter Location">
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputPassword4">Email Address</label>
+              <input type="text" class="form-control" id="email" placeholder="Enter Email Address">
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputPassword4">Phone Number</label>
+              <input type="text" class="form-control" id="phone_number" placeholder="Enter Phone Number">
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputPassword4">Gender</label>
+              <select class="form-control select2" id="gender">
+                <option value=""></option>
+                <option value="Male"> Male </option>
+                <option value="Female"> Female </option>
+              </select>
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputPassword4">Position</label>
+              <select class="form-control position" id="position">
+                <option value=""></option>
+                <option value="Admin"> Admin </option>
+                <option value="Staff"> Staff </option>
+              </select>
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputPassword4">Date of Birth</label>
+              <input type="text" class="form-control datepicker" id="dob" placeholder="MM/DD/YYYY">
+            </div>
+          </div>
+      </div>
+      <div class="modal-footer bg-whitesmoke br">
+        <button type="button" id="save_staff" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Modal for save staff end here -->
+
+<!-- Modal for update staff start -->
+<div class="modal fade" id="edit_staff" tabindex="-1" role="dialog" aria-labelledby="EditStaffModal"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="EditStaffModal">Staff Information</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="form-row">
+            <div class="form-group col-md-4">
+              <label for="inputEmail4">First Name</label>
+              <input type="text" class="form-control" id="" placeholder="Enter First Name">
+            </div>
+            <div class="form-group col-md-4">
+              <label for="inputPassword4">Middle Name</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Middle Name">
+            </div>
+            <div class="form-group col-md-4">
+              <label for="inputPassword4">Last Name</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Last Name">
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputPassword4">Location</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Location">
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputPassword4">Email Address</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Email Address">
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputPassword4">Phone Number</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Phone Number">
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputPassword4">Gender</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Gender">
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputPassword4">Position</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Position">
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputPassword4">Date of Birth</label>
+              <input type="text" class="form-control datepicker" id="" placeholder="Choose Date of Birth">
+            </div>
+          </div>
+      </div>
+      <div class="modal-footer bg-whitesmoke br">
+        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal for staff end here -->
+
+<!-- Modal for Product start here -->
+<div class="modal fade" id="product" tabindex="-1" role="dialog" aria-labelledby="ProductModal"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="ProductModal">Product Information</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="form-row">
+            <div class="form-group col-md-6">
+              <label for="inputEmail4">Product Name</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Product Name">
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputPassword4">Product Details</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Middle Name">
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputPassword4">Buyying Price</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Last Name">
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputPassword4">Selling Price</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Location">
+            </div>
+            <div class="form-group col-md-12">
+              <label for="inputPassword4">Product Image</label>
+              <input type="file" class="form-control" id="" placeholder="Enter Email Address">
+            </div>
+          </div>
+      </div>
+      <div class="modal-footer bg-whitesmoke br">
+        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Modal for save Product end here -->
+
+<!-- Modal for update Product start -->
+<div class="modal fade" id="edit_product" tabindex="-1" role="dialog" aria-labelledby="EditProductModal"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="EditProductModal">Product Information</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="form-row">
+        <div class="form-group col-md-6">
+              <label for="inputEmail4">Product Name</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Product Name">
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputPassword4">Product Details</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Middle Name">
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputPassword4">Buyying Price</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Last Name">
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputPassword4">Selling Price</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Location">
+            </div>
+            <div class="form-group col-md-12">
+              <label for="inputPassword4">Product Image</label>
+              <input type="file" class="form-control" id="" placeholder="Enter Email Address">
+            </div>
+          </div>
+      </div>
+      <div class="modal-footer bg-whitesmoke br">
+        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal for Product end here -->
+
+<!-- Modal for Instruction start here -->
+<div class="modal fade" id="instruction" tabindex="-1" role="dialog" aria-labelledby="InstructionModal"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="InstructionModal">Instruction Information</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="form-row">
+            <div class="form-group col-md-4">
+              <label for="inputEmail4">Title</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Instruction Name">
+            </div>
+            <div class="form-group col-md-4">
+              <label for="inputPassword4">Requerment</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Middle Name">
+            </div>
+            <div class="form-group col-md-4">
+              <label for="inputPassword4">Details</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Last Name">
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputPassword4">Video</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Location">
+            </div>
+          </div>
+      </div>
+      <div class="modal-footer bg-whitesmoke br">
+        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Modal for save Instruction end here -->
+
+<!-- Modal for update Instruction start -->
+<div class="modal fade" id="edit_instruction" tabindex="-1" role="dialog" aria-labelledby="EditInstructionModal"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="EditInstructionModal">Instruction Information</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="form-row">
+        <div class="form-group col-md-4">
+              <label for="inputEmail4">Title</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Instruction Name">
+            </div>
+            <div class="form-group col-md-4">
+              <label for="inputPassword4">Requerment</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Middle Name">
+            </div>
+            <div class="form-group col-md-4">
+              <label for="inputPassword4">Details</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Last Name">
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputPassword4">Video</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Location">
+            </div>
+          </div>
+      </div>
+      <div class="modal-footer bg-whitesmoke br">
+        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal for Instruction end here -->
+
+<!-- Modal for Position start here -->
+<div class="modal fade" id="position" tabindex="-1" role="dialog" aria-labelledby="PositionModal"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="PositionModal">Position Information</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="form-row">
+            <div class="form-group col-md-4">
+              <label for="inputEmail4">Position Name</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Position Name">
+            </div>
+            <div class="form-group col-md-4">
+              <label for="inputPassword4">Position Prefix</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Position Prefix">
+            </div>
+          </div>
+      </div>
+      <div class="modal-footer bg-whitesmoke br">
+        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Modal for save Position end here -->
+
+<!-- Modal for update Position start -->
+<div class="modal fade" id="edit_Position" tabindex="-1" role="dialog" aria-labelledby="EditPositionModal"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="EditPositionModal">Position Information</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="form-row">
+        <div class="form-group col-md-4">
+              <label for="inputEmail4">Position Name</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Position Name">
+            </div>
+            <div class="form-group col-md-4">
+              <label for="inputPassword4">Position Prefix</label>
+              <input type="text" class="form-control" id="" placeholder="Enter Position Prefix">
+            </div>
+          </div>
+      </div>
+      <div class="modal-footer bg-whitesmoke br">
+        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal for Position end here -->
